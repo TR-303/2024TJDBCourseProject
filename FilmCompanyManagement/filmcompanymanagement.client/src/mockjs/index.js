e1223a0804d0b26f08be0907b02e9d2e6a5e8895
zyzzzmr <93696071+zyzzzmr@users.noreply.github.com>
2024-09-02 10:38:28 +0800

    Add files via upload

---
+++ FilmCompanyManagement/filmcompanymanagement.client/src/mockjs/index.js
@@ -0,0 +1,149 @@
+﻿import Mock from 'mockjs'
+//infopage用
+let dataList = [
+    { id: '1', name: 'boss' },
+    { id: '2', name: 'manager' },
+    { id: '3', name: 'worker' }
+];
+//login用
+let loginList = [
+    { username: '1', password: '1', department: '管理部' },
+    { username: '2', password: '2', department: '财务部' },
+    { username: '3', password: '3', department: '业务部' },
+]
+//department用
+
+let signList = [
+    { id: '1', issign: '0', signtime: '' },
+    { id: '2', issign: '0', signtime: '' },
+    { id: '3', issign: '0', signtime: '' },
+]
+let userdataList = [
+    { id: '1', department: '管理部', phone: '12345' },
+    { id: '2', department: '财务部', phone: '67890' },
+    { id: '3', department: '业务部', phone: '13579' },
+    { id: '4', department: '业务部', phone: '22222' },
+] 
+//传了id
+Mock.mock('/data/userdata', 'post', (params) => {
+    let user = JSON.parse(params.body);
+    const userdata = dataList.find(item => item.id === user.id);
+    return {
+        name: userdata.name
+    };
+}
+)
+
+//传了username，password，department
+Mock.mock('api/Login/IsUserUni', 'post', (params) => {
+    let login = JSON.parse(params.body);
+    const logindata = loginList.find(item => item.username === login.username && item.password === login.password && item.department === login.department);
+    if (logindata)
+        return {
+            department: logindata.department,
+            success: 1,
+        };
+    else
+        return {
+            success:0,
+        }
+}
+)
+
+//传了id和time
+Mock.mock('/data/signdata', 'post', (params) => {
+    let user = JSON.parse(params.body);
+    const signdata = signList.find(item => item.id === user.id);
+    if (signdata.issign === '0') {
+        signdata.issign = '1';
+        signdata.signtime = user.time;
+        console.log(signdata);
+        return {
+            signtime: signdata.signtime,
+            success: 1,
+        }
+    }
+    else
+        return {
+            success: 0,
+            signtime: signdata.signtime,
+        }
+}
+)
+
+//传了department
+Mock.mock('/data/departmentuserdata', 'post',(params)=> {
+    let data = JSON.parse(params.body);
+    const departmentdata = userdataList.filter(item => item.department === data.department);
+    if (departmentdata) {
+        console.log(departmentdata);
+        return departmentdata;
+    }
+}
+)
+
+
+//worker
+
+
+Mock.mock('/api/workers', 'get', {
+    projects: [{
+        workerID: '3'
+    }]
+})
+
+Mock.mock('/api/projects', 'get', {
+    projects: [{
+        id: '001',
+        managerID: '2150001',
+        orderDate: '2024.7.1',
+        orderStatus: '进行中',
+        paymentStatus: '进行中',
+        workerID: '3'
+    }, {
+        id: '002',
+        managerID: '2150002',
+        orderDate: '2024.7.2',
+        orderStatus: '进行中',
+        paymentStatus: '进行中',
+        workerID: '3'
+    }],
+})
+
+Mock.mock('/api/expense', 'post', {
+    message: '经费申请已提交'
+})
+
+Mock.mock('/api/purchase', 'post', {
+    message: '设备购买已提交'
+})
+
+Mock.mock('/api/repair', 'post', {
+    message: '设备维修已提交'
+})
+
+Mock.mock('/api/files', 'get', {
+    files: [{
+        id: '001',
+        name: '入职通知书',
+        type: 'pdf',
+        description: '欢迎新人！',
+        size: '114 KB',
+        uploadDate: '2024.7.1',
+        status: '收件人',
+        workerID: '3'
+    }, {
+        id: '002',
+        name: '解职通知书',
+        type: 'pdf',
+        description: '你被开了！',
+        size: '514 KB',
+        uploadDate: '2024.7.1',
+        status: '收件人',
+        workerID: '3'
+    },],
+})
+
+Mock.mock('/api/upload', 'post', {
+    message: '文件已上传'
+})
