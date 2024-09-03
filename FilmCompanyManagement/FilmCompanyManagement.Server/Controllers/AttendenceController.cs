19711859f5f7ffca901243d90ca60bd0c39bba90
王智 <1848195475@qq.com>
2024-09-03 10:54:30 +0800

    考勤板块

--- FilmCompanyManagement/FilmCompanyManagement.Server/Controllers/AttendenceController.cs
+++ FilmCompanyManagement/FilmCompanyManagement.Server/Controllers/AttendenceController.cs
@@ -26,8 +26,38 @@ namespace FilmCompanyManagement.Controllers
         [HttpPost]
         public async Task<string> IsAttended(string userName)
         {
-            var attendances = await _context.Attendances.Where(a => a.Employee.UserName == userName).ToListAsync();
-            return attendances.Count == 0 ? "false" : "true";
+            var attendance = await _context.Attendances.Where(a => a.Employee.UserName == userName).SingleAsync();
+            return attendance == null ? "false" : "true";
+        }
+
+        [HttpPost]
+        public async Task<string> InsertAttendenceRecord(string userName, string time, int state)
+        {
+            await Task.Run(() =>
+            {
+                var times = time.Split(' ');
+                var date = Convert.ToDateTime(times[0]);
+                var accurateTime = TimeSpan.Parse(times[1]);
+                var attendance = _context.Attendances.Where(a => a.Employee.UserName == userName && a.Date = date).SingleAsync();
+                if (attendance == null)
+                {
+                    attendance = new Attendance
+                    {
+                        Date = date;
+                        IsLate = 0;
+                        IsEarlyLeave = 0;
+                        IsOnLeave = 0;
+                        IsOvertime = 0;
+                    };
+                    _context.AddAsync(attendance);
+                }
+                if (state == 1)//上班
+                    attendance.CheckInTime = accurateTime;
+                else if (state == 0)//下班
+                    attendance.CheckOutTime = accurateTime;
+                _context.SaveChanfesAsync();
+        `   });
+            return "1";
         }
     }
 }
