de2d5a8f53d6e61cac3a4d1eedafe6e5e8e357dd
TR-303 <112552988+TR-303@users.noreply.github.com>
2024-09-06 23:11:50 +0800

    datacontroller

--- FilmCompanyManagement/FilmCompanyManagement.Server/Migrations/20240906145600_update6.Designer.cs
+++ FilmCompanyManagement/FilmCompanyManagement.Server/Migrations/20240906145600_update6.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FilmCompanyManagement.Server.EntityFrame;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Oracle.EntityFrameworkCore.Metadata;
 
@@ -11,9 +12,11 @@ using Oracle.EntityFrameworkCore.Metadata;
 namespace FilmCompanyManagement.Server.Migrations
 {
     [DbContext(typeof(FCMDbContext))]
-    partial class FCMDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240906145600_update6")]
+    partial class update6
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FilmCompanyManagement/FilmCompanyManagement.Server/Migrations/20240906145600_update6.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmCompanyManagement.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class update6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FileType",
+                table: "Files",
+                type: "NVARCHAR2(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FileType",
+                table: "Files",
+                type: "NVARCHAR2(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
