373834b29be1065a9c67050907cc51476f19411f
王智 <1848195475@qq.com>
2024-09-06 20:28:14 +0800

    完成部门相关查询

--- FilmCompanyManagement/FilmCompanyManagement.Server/Program.cs
+++ FilmCompanyManagement/FilmCompanyManagement.Server/Program.cs
@@ -12,7 +12,8 @@ namespace FilmCompanyManagement.Server
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers()
+                .AddJsonOptions(options => {options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;});
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
